cf1d44eb94edb1e38eb0edbf61013c3d5c7788a9
korayyalcin1903 <korayyalcin1903@gmail.com>
2023-05-02 19:54:34 +0300

    first commit

--- js/script.js
+++ js/script.js
@@ -1,6 +1,28 @@
 const button = document.querySelector(".nav-menu-button")
 const menu = document.querySelector(".navbar-bottom .container")
 
+const sections = document.querySelectorAll('section')
+
+var observer = new IntersectionObserver((entries) => {
+    entries.forEach(entry => {
+        entry.target.classList.toggle("slipping", entry.isIntersecting)
+    })
+}) 
+
+sections.forEach(section => {
+    observer.observe(section)
+})
+
+// let serviceHeight = service.getBoundingClientRect().height
+
+// window.addEventListener("scroll", () => {
+//     console.log(serviceHeight)
+//     console.log(window.scrollY)
+//     if(Math.ceil(serviceHeight) == window.scrollY - 50){
+//         service.classList.add("slipping")
+//     }
+// })
+
 let sayac = 0
 
 button.addEventListener("click", () => {
